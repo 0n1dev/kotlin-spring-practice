541407e01c4c8cccc6c967c1cf052cc753b834f6
0n1dev <all_mind@naver.com>
2022-01-16 00:24:09 +0900

    feat: Spring Validation PutApiController 수정

--- src/main/kotlin/com/example/kotlinspringpractice/controller/put/PutApiController.kt
+++ src/main/kotlin/com/example/kotlinspringpractice/controller/put/PutApiController.kt
@@ -1,9 +1,11 @@
 package com.example.kotlinspringpractice.controller.put
 
-import com.example.kotlinspringpractice.model.http.Result
 import com.example.kotlinspringpractice.model.http.UserRequest
-import com.example.kotlinspringpractice.model.http.UserResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api")
@@ -20,36 +22,52 @@ class PutApiController {
     }
 
     @PutMapping(path = ["/put-mapping/object"])
-    fun putMappingObject(@RequestBody userRequest: UserRequest): UserResponse {
-        // 0. Response
-        return UserResponse().apply {
+    fun putMappingObject(@Valid @RequestBody userRequest: UserRequest,
+                         bindingResult: BindingResult): ResponseEntity<String> {
 
-            // 1. result
-            Result().apply {
-                this.resultCode = "OK"
-                this.resultMessage = "성공"
+        if (bindingResult.hasErrors()) {
+
+            val msg = StringBuilder()
+            bindingResult.allErrors.forEach {
+                val field = it as FieldError
+                val message = it.defaultMessage
+
+                msg.append("${field.field} $message\n")
             }
 
-
-        }.apply {
-
-            // 2. description
-            this.description = "@@@@@@"
-        }.apply {
-
-            // 3. user mutable list
-            val userList = mutableListOf<UserRequest>()
-
-            userList.add(userRequest)
-            userList.add(UserRequest().apply {
-                this.name = "a"
-                this.age = 11
-                this.email = "test@naver.com"
-                this.address = "dd"
-                this.phoneNumber = "01018838303"
-            })
-
-            this.userRequest = userList
+            return ResponseEntity.badRequest().body(msg.toString())
         }
+
+//        // 0. Response
+//        return UserResponse().apply {
+//
+//            // 1. result
+//            Result().apply {
+//                this.resultCode = "OK"
+//                this.resultMessage = "성공"
+//            }
+//
+//
+//        }.apply {
+//
+//            // 2. description
+//            this.description = "@@@@@@"
+//        }.apply {
+//
+//            // 3. user mutable list
+//            val userList = mutableListOf<UserRequest>()
+//
+//            userList.add(userRequest)
+//            userList.add(UserRequest().apply {
+//                this.name = "a"
+//                this.age = 11
+//                this.email = "test@naver.com"
+//                this.address = "dd"
+//                this.phoneNumber = "01018838303"
+//            })
+//
+//            this.userRequest = userList
+//        }
+        return ResponseEntity.ok("")
     }
 }
--- src/main/kotlin/com/example/kotlinspringpractice/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/kotlinspringpractice/model/http/UserRequest.kt
@@ -4,15 +4,42 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import javax.validation.constraints.*
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 // @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class) PropertyNamingStrategy Class Deprecated
 data class UserRequest(
+    // @Size(min = 2, max = 8) // 자바에서 사용하는 방식
+    @field:NotEmpty
+    @field:Size(min = 2, max = 8) // 코틀린에서 Data Class 필드에 어노테이션을 붙일 때
     var name: String?=null,
+
+    @field:PositiveOrZero // 0 이상의 숫자를 검증
     var age: Int?=null,
+
+    @field:Email // email 양식
     var email: String?=null,
+
+    @field:NotBlank // 공백을 검증
     var address: String?=null,
 
+    @field:Pattern(regexp = """^\d{2,3}-\d{3,4}-\d{4}$""") // 정규식 검증
     // @JsonProperty("phone_number") Json에서는 스네이크케이스를 주로 사용해서 서로 매핑되도록 지정.
-    var phoneNumber: String?=null // 코틀린에서는 기본적으로 카멜케이스 사용.
-)
+    var phoneNumber: String?=null, // 코틀린에서는 기본적으로 카멜케이스 사용.
+
+    var createdAt: String?=null
+) {
+
+    @AssertTrue(message = "createdAt의 패턴이 잘못되었습니다.")
+    private fun isValidCreatedAt(): Boolean {
+
+        return try {
+            LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
