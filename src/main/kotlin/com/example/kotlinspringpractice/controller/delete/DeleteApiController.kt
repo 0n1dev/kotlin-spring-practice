ec2c58fbd50c28423ba053794e60c37f56d477d5
0n1dev <all_mind@naver.com>
2022-01-16 00:06:10 +0900

    feat: Spring Validation 추가

--- src/main/kotlin/com/example/kotlinspringpractice/controller/delete/DeleteApiController.kt
+++ src/main/kotlin/com/example/kotlinspringpractice/controller/delete/DeleteApiController.kt
@@ -1,9 +1,14 @@
 package com.example.kotlinspringpractice.controller.delete
 
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
 
 @RestController
 @RequestMapping("/api")
+@Validated
 class DeleteApiController {
 
     // 1. path variable
@@ -11,8 +16,15 @@ class DeleteApiController {
 
     @DeleteMapping(path = ["/delete-mapping/name/{name}/age/{age}"])
     fun deleteMappingPath(
-        @PathVariable name: String,
-        @PathVariable age: Int
+        @PathVariable
+        @NotNull
+        @Size(min = 1, max = 10)
+        name: String,
+
+        @NotNull(message = "age 값이 누락되었습니다.")
+        @Min(value = 20, message = "age는 20보다 커야 합니다.")
+        @PathVariable
+        age: Int
     ): String {
         return "$name $age"
     }
